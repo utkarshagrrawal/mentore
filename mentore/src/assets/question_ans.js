71113f9268507e687d9e79eae5682a463e41ce82
SatyamJha2151 <satyamjha2151@gmail.com>
2024-02-14 17:07:06 +0530

    updation of stories

--- mentore/src/assets/question_ans.js
+++ mentore/src/assets/question_ans.js
@@ -1,22 +1,25 @@
 export const questions = [
-    {
-      id: 1,
-      question: 'Question 1: What is React?',
-      answer: 'React is a JavaScript library for building user interfaces.'
-    },
-    {
-      id: 2,
-      question: 'Question 2: How do you create a React component?',
-      answer: 'You can create a React component by defining a JavaScript function or class.'
-    },
-    {
-      id: 3,
-      question: 'Question 3: What is state in React?',
-      answer: 'State is a built-in object in React used for managing component data.'
-    },
-    {
-      id: 4,
-      question: 'Question 4: What is props in React?',
-      answer: 'Props (short for properties) are a way to pass data from parent to child components.'
-    }
-  ]
+  {
+    id: 1,
+    question: "Question 1: Does Long-term mentorship really produce outcomes?",
+    answer: "React is a JavaScript library for building user interfaces.",
+  },
+  {
+    id: 2,
+    question: "Question 2: How do you create a React component?",
+    answer:
+      "You can create a React component by defining a JavaScript function or class.",
+  },
+  {
+    id: 3,
+    question: "Question 3: What is state in React?",
+    answer:
+      "State is a built-in object in React used for managing component data.",
+  },
+  {
+    id: 4,
+    question: "Question 4: What is props in React?",
+    answer:
+      "Props (short for properties) are a way to pass data from parent to child components.",
+  },
+];
--- mentore/src/assets/stories.js
+++ mentore/src/assets/stories.js
@@ -1,56 +1,65 @@
 export const data = [
-    {
-      id: 1,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget mauris eget justo eleifend vestibulum vel a metus."
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      img: "https://images.unsplash.com/photo-1544168190-79c17527004f?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Integer eget consectetur lectus. Nam mattis, enim non finibus interdum, magna sapien vestibulum eros, at faucibus purus libero sit amet nunc."
-    },
-    {
-      id: 3,
-      name: "Raj Jain",
-      img: "https://images.unsplash.com/photo-1615263787251-a9a7c6c26cf1?q=80&w=1883&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec ac ligula eu nisl consequat rutrum."
-    },
-    {
-      id: 4,
-      name: "Bob Brown",
-      img: "https://images.unsplash.com/photo-1601392561050-340745ba9c25?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Vestibulum nec odio at sapien lobortis tristique. Vivamus suscipit tellus ac elit fermentum, vel mollis ex luctus."
-    },
-    {
-      id: 5,
-      name: "Emily Williams",
-      img: "https://images.unsplash.com/photo-1544717305-2782549b5136?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Maecenas eu augue sem. Nullam sit amet massa tellus. Fusce a turpis ut eros consequat sagittis."
-    },
-    {
-      id: 6,
-      name: "Sarah Wilson",
-      img: "https://images.unsplash.com/photo-1567662810010-05f4f9896cb2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Duis non purus vitae purus varius malesuada. Proin elementum risus in magna volutpat, a tincidunt ipsum laoreet."
-    },
-    {
-      id: 7,
-      name: "David Martinez",
-      img: "https://images.unsplash.com/photo-1543060829-a0029874b174?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Aenean quis erat vel elit mattis tincidunt. Aliquam sit amet tortor id diam sodales condimentum."
-    },
-    {
-      id: 8,
-      name: "Olivia Garcia",
-      img: "https://images.unsplash.com/photo-1571863393474-b94c72f117fe?q=80&w=1977&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Suspendisse potenti. Nam rutrum, dui sed consequat finibus, enim urna tincidunt quam, sit amet aliquam eros leo vel mi."
-    },
-    {
-      id: 9,
-      name: "William Rodriguez",
-      img: "https://images.unsplash.com/photo-1623666996666-6a71ec53ff42?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      review: "Phasellus commodo, justo a tempus auctor, quam ipsum bibendum metus, eu luctus arcu libero vel libero."
-    }
-  ];
+  {
+    id: 1,
+    name: "John Doe",
+    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    review:
+      "Mentore transformed my software development career with expert guidance on real-world projects and deepened my understanding of software architecture.",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    img: "https://images.unsplash.com/photo-1544168190-79c17527004f?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore was invaluable in my data science journey, offering personalized mentorship to navigate complex algorithms and apply machine learning techniques to real-world datasets.",
+  },
+  {
+    id: 3,
+    name: "Raj Jain",
+    img: "https://images.unsplash.com/photo-1615263787251-a9a7c6c26cf1?q=80&w=1883&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore's web development mentors empowered me with industry insights and best practices, transforming my web development skills.",
+  },
+  {
+    id: 4,
+    name: "Bob Brown",
+    img: "https://images.unsplash.com/photo-1601392561050-340745ba9c25?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore played a crucial role in my cybersecurity career, covering topics from ethical hacking to secure coding practices.",
+  },
+  {
+    id: 5,
+    name: "Emily Williams",
+    img: "https://images.unsplash.com/photo-1544717305-2782549b5136?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore's machine learning mentors helped me bridge the gap between theory and application, shaping me into a proficient machine learning engineer.",
+  },
+  {
+    id: 6,
+    name: "Sarah Wilson",
+    img: "https://images.unsplash.com/photo-1567662810010-05f4f9896cb2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore was instrumental in refining my UI/UX design skills, covering design principles, prototyping techniques, and industry trends.",
+  },
+  {
+    id: 7,
+    name: "David Martinez",
+    img: "https://images.unsplash.com/photo-1543060829-a0029874b174?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore's full-stack development mentors significantly accelerated my understanding of both front-end and back-end technologies.",
+  },
+  {
+    id: 8,
+    name: "Olivia Garcia",
+    img: "https://images.unsplash.com/photo-1571863393474-b94c72f117fe?q=80&w=1977&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore provided a comprehensive DevOps mentorship, transforming my approach to infrastructure and automation.",
+  },
+  {
+    id: 9,
+    name: "William Rodriguez",
+    img: "https://images.unsplash.com/photo-1623666996666-6a71ec53ff42?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    review:
+      "Mentore's blockchain development mentors provided a deep dive into decentralized applications and smart contract development.",
+  },
+];
