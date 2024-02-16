7737156424a94862017c453ad2c4c9d081271506
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-02-16 16:53:34 +0530

    Updates padding in navbar

--- mentore/components/liveWebinars.jsx
+++ mentore/components/liveWebinars.jsx
@@ -40,7 +40,7 @@ export function LiveWebinars() {
   return (
     <div className='min-h-screen items-center flex flex-col w-full'>
       <div className='w-full bg-[#d2d2d217]'>
-        <div className='flex flex-wrap justify-center items-center md:justify-between lg:justify-between mx-16 my-6'>
+        <div className='flex flex-wrap justify-center items-center md:justify-between lg:justify-between mx-16 my-3'>
           <Link to='/'><img src="../static/logo.png" className="h-8 mix-blend-multiply" alt="Mentore" /></Link>
           <input type='search' placeholder='Search for mentors' className='lg:w-[40rem] md:w-[26rem] w-[16rem] border-2 border-blue-700 rounded-lg px-4 py-2 focus:outline-none focus:ring-4 focus:ring-blue-300' />
           <button onClick={handleLogButton} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-3'>{loggedIn ? 'Dashboard' : 'Login'}</button>
