1bdbe1fa3df0a209f07ee9077674201982bfec38
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-02-23 12:17:54 +0530

    Updates responsiveness in login page

--- mentore/components/login.jsx
+++ mentore/components/login.jsx
@@ -5,166 +5,166 @@ import Swal from 'sweetalert2';
 import { Loader } from './loader';
 
 export function Login() {
-  const [login, setLogin] = useState({ email: '', password: '' });
-  const [eyeState, setEyeState] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate()
+    const [login, setLogin] = useState({ email: '', password: '' });
+    const [eyeState, setEyeState] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
 
-  const handleChange = (e) => {
-    setLogin({ ...login, [e.target.name]: e.target.value });
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    let loginUser = await fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(login)
-    })
-    let result = await loginUser.json();
-    if (result.success) {
-      Swal.fire({
-        icon: 'success',
-        title: 'Logged in successfully',
-        showConfirmButton: false,
-        timer: 1500
-      })
-      localStorage.setItem('token', result.token);
-      localStorage.setItem('name', result.name);
-      localStorage.setItem('email', login.email);
-      navigate('/');
-    } else {
-      Swal.fire(
-        'Error',
-        result.error,
-        'error'
-      )
+    const handleChange = (e) => {
+        setLogin({ ...login, [e.target.name]: e.target.value });
     }
-    setLoading(false);
-  }
 
-  useEffect(() => {
-    if (localStorage.getItem('token') === null) {
-      return;
-    }
-    const getUser = async () => {
-      let user = await fetch('http://localhost:3000/getcurrentuser', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': localStorage.getItem('token')
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        let loginUser = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(login)
+        })
+        let result = await loginUser.json();
+        if (result.success) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Logged in successfully',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            localStorage.setItem('token', result.token);
+            localStorage.setItem('name', result.name);
+            localStorage.setItem('email', login.email);
+            navigate('/');
+        } else {
+            Swal.fire(
+                'Error',
+                result.error,
+                'error'
+            )
         }
-      });
-      let result = await user.json();
-      if (result.error) {
-        localStorage.removeItem('token');
-        localStorage.removeItem('name');
-        localStorage.removeItem('email');
-        return;
-      } else {
-        navigate('/');
-      }
+        setLoading(false);
     }
-    getUser();
-  }, [])
 
-  const handleEyeChange = () => {
-    setEyeState(!eyeState);
-    if (eyeState) {
-      document.getElementById('password').type = 'text';
-    } else {
-      document.getElementById('password').type = 'password';
+    useEffect(() => {
+        if (localStorage.getItem('token') === null) {
+            return;
+        }
+        const getUser = async () => {
+            let user = await fetch('http://localhost:3000/getcurrentuser', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }
+            });
+            let result = await user.json();
+            if (result.error) {
+                localStorage.removeItem('token');
+                localStorage.removeItem('name');
+                localStorage.removeItem('email');
+                return;
+            } else {
+                navigate('/');
+            }
+        }
+        getUser();
+    }, [])
+
+    const handleEyeChange = () => {
+        setEyeState(!eyeState);
+        if (eyeState) {
+            document.getElementById('password').type = 'text';
+        } else {
+            document.getElementById('password').type = 'password';
+        }
     }
-  }
 
-  const loginCode = (
-    <div className="border-2 border-black mt-3 py-4 w-1/3 flex flex-col bg-white rounded-xl">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
-      </div>
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                onChange={handleChange}
-                value={login.email}
-                required
-                className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+    const loginCode = (
+        <div className="border-2 border-black mt-3 px-4 py-4 w-full md:w-1/2 lg:w-1/3 flex flex-col bg-white rounded-xl">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
             </div>
-          </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                onChange={handleChange}
+                                value={login.email}
+                                required
+                                className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Password
-              </label>
-              <div className="text-sm">
-                <Link to="/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </Link>
-              </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <Link to="/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                    Forgot your password?
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <div className='flex items-center relative'>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    onChange={handleChange}
+                                    value={login.password}
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {eyeState ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 absolute right-3" onClick={handleEyeChange}>
+                                        <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                                        <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" clipRule="evenodd" />
+                                    </svg>
+                                ) :
+                                    (
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 absolute right-3" onClick={handleEyeChange}>
+                                            <path fillRule="evenodd" d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-4.38 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z" clipRule="evenodd" />
+                                            <path d="m10.748 13.93 2.523 2.523a9.987 9.987 0 0 1-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 0 1 0-1.186A10.007 10.007 0 0 1 2.839 6.02L6.07 9.252a4 4 0 0 0 4.678 4.678Z" />
+                                        </svg>
+                                    )}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                    <p className="text-sm text-center text-gray-600">
+                        Don't have an account? <Link to="/register" className="font-semibold text-indigo-600 hover:text-indigo-500 underline">Register</Link>
+                    </p>
+                </form>
             </div>
-            <div className="mt-2">
-              <div className='flex items-center relative'>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  onChange={handleChange}
-                  value={login.password}
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                {eyeState ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 absolute right-3" onClick={handleEyeChange}>
-                    <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                    <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" clipRule="evenodd" />
-                  </svg>
-                ) :
-                  (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 absolute right-3" onClick={handleEyeChange}>
-                      <path fillRule="evenodd" d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-4.38 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z" clipRule="evenodd" />
-                      <path d="m10.748 13.93 2.523 2.523a9.987 9.987 0 0 1-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 0 1 0-1.186A10.007 10.007 0 0 1 2.839 6.02L6.07 9.252a4 4 0 0 0 4.678 4.678Z" />
-                    </svg>
-                  )}
-              </div>
-            </div>
-          </div>
+        </div>
+    )
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-          <p className="text-sm text-center text-gray-600">
-            Don't have an account? <Link to="/register" className="font-semibold text-indigo-600 hover:text-indigo-500 underline">Register</Link>
-          </p>
-        </form>
-      </div>
-    </div>
-  )
-
-  return (
-    <div className='bg-gradient-to-r from-blue-300 via-gray-300 to-yellow-300 flex min-h-screen flex-col items-center justify-center px-6 py-12 lg:px-8'>
-      <Link to='/' className='relative'><img src="../static/logo.png" className="h-12 mix-blend-multiply" alt="Mentore" /></Link>
-      {loading ? <Loader /> : loginCode}
-    </div>
-  )
+    return (
+        <div className='bg-gradient-to-r from-blue-300 via-gray-300 to-yellow-300 flex min-h-screen flex-col items-center justify-center px-6 py-12 lg:px-8'>
+            <Link to='/' className='relative'><img src="../static/logo.png" className="h-12 mix-blend-multiply" alt="Mentore" /></Link>
+            {loading ? <Loader /> : loginCode}
+        </div>
+    )
 }
