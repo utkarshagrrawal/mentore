0d7d19dc4908d0618c469add072b50814337f520
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-02-24 14:21:48 +0530

    Updates like functionality in insights page

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,94 +1,104 @@
 const { supabase } = require("../utility/dbConnection");
 
 const getBlogs = async (req, res) => {
-  const { data, error } = await supabase
-    .from("blogs")
-    .select()
-    .eq("email", req.user.email);
-  if (!error) {
-    return res.json({ result: data });
-  }
-  return res.json({ error: error.message });
+    const { data, error } = await supabase
+        .from("blogs")
+        .select()
+        .eq("email", req.user.email);
+    if (!error) {
+        return res.json({ result: data });
+    }
+    return res.json({ error: error.message });
 };
 
 const getAllBlogs = async (req, res) => {
-  const { data, error } = await supabase.from("blogs").select();
-  if (!error) {
-    return res.json({ result: data });
-  }
-  return res.json({ error: error.message });
+    const { data, error } = await supabase.from("blogs").select();
+    if (!error) {
+        return res.json({ result: data });
+    }
+    return res.json({ error: error.message });
 };
 
 const createBlog = async (req, res) => {
-  const { title, content } = req.body;
-  const { error } = await supabase.from("blogs").insert({
-    title: title,
-    content: content,
-    email: req.user.email,
-    name: req.user.name,
-    likes: 0,
-    comments: [],
-  });
-  if (!error) {
-    return res.json({ success: "Blog created successfully!" });
-  }
-  return res.json({ error: error.message });
+    const { title, content } = req.body;
+    const { error } = await supabase.from("blogs").insert({
+        title: title,
+        content: content,
+        email: req.user.email,
+        name: req.user.name,
+        likes: 0,
+        comments: [],
+    });
+    if (!error) {
+        return res.json({ success: "Blog created successfully!" });
+    }
+    return res.json({ error: error.message });
 };
 
 const getCurrentBlog = async (req, res) => {
-  const { blogID } = req.body;
-  const { data, error } = await supabase
-    .from("blogs")
-    .select()
-    .eq("id", blogID);
-  if (!error) {
-    return res.json({ result: data[0] });
-  }
-  return res.json({ error: error.message });
+    const { blogID } = req.body;
+    const { data, error } = await supabase
+        .from("blogs")
+        .select()
+        .eq("id", blogID);
+    if (!error) {
+        return res.json({ result: data[0] });
+    }
+    return res.json({ error: error.message });
 };
 
 const deleteBlog = async (req, res) => {
-  const { id } = req.body;
-  const { error } = await supabase.from("blogs").delete().eq("id", id);
-  if (!error) {
-    return res.json({ success: "Blog deleted successfully!" });
-  } else {
-    return res.json({ error: error.message });
-  }
+    const { id } = req.body;
+    const { error } = await supabase.from("blogs").delete().eq("id", id);
+    if (!error) {
+        return res.json({ success: "Blog deleted successfully!" });
+    } else {
+        return res.json({ error: error.message });
+    }
 };
 
 const addLike = async (req, res) => {
-  const { blogID } = req.body;
-  const { error } = await supabase
-    .from("blogs")
-    .update({ likes: supabase.sql`likes + 1` })
-    .eq("id", blogID);
-  if (!error) {
-    return res.json({ success: "Like added successfully!" });
-  }
-  return res.json({ error: error.message });
+    const { blogID } = req.body;
+    const { data, error } = await supabase
+        .from("blogs")
+        .select("likes")
+        .eq("id", blogID);
+
+    if (error) {
+        return res.json({ error: error.message });
+    }
+
+    const { error: newError } = await supabase
+        .from("blogs")
+        .update({ likes: data[0].likes + 1 })
+        .eq("id", blogID);
+
+    if (!newError) {
+        return res.json({ success: "Like added successfully!" });
+    }
+    return res.json({ error: newError.message });
 };
 
 const addComment = async (req, res) => {
-  const { blogID, comment } = req.body;
-  const { error } = await supabase
-    .from("blogs")
-    .update({ comments: supabase.sql`comments || ${[comment]}` })
-    .eq("id", blogID);
+    const { blogID, comment } = req.body;
+    const { error } = await supabase
+        .from("blogs")
+        .update({ comments: supabase.sql`comments || ${[comment]}` })
+        .eq("id", blogID);
 
-  if (!error) {
-    return res.json({ success: "Comment added successfully!" });
-  }
+    if (!error) {
+        return res.json({ success: "Comment added successfully!" });
+    }
 
-  return res.json({ error: error.message });
+    return res.json({ error: error.message });
 };
 
 module.exports = {
-  getBlogs,
-  getAllBlogs,
-  createBlog,
-  getCurrentBlog,
-  deleteBlog,
-  addLike,
-  addComment,
+    getBlogs,
+    getAllBlogs,
+    createBlog,
+    getCurrentBlog,
+    deleteBlog,
+    addLike,
+    addComment,
 };
--- server/server.js
+++ server/server.js
@@ -1,74 +1,28 @@
-<<<<<<< Updated upstream
 const express = require('express')
 const cors = require('cors');
 const { registerUser, loginUser, forgotPassword, getcurrentuser, verifyOtp, resendOtp, changepassword, logout } = require('./controllers/userController');
 const { authentication } = require('./utility/passportUtility');
 const { getAllSkills, getMentorDetails, getAllMentors, getMentorProfile } = require('./controllers/mentorController');
 const { createWebinar, getWebinars, getAllWebinars, addParticipant, addHost } = require('./controllers/dyteController');
-const { getBlogs, createBlog, getCurrentBlog, getAllBlogs, deleteBlog } = require('./controllers/blogController');
+const { getBlogs, createBlog, getCurrentBlog, getAllBlogs, deleteBlog, addLike } = require('./controllers/blogController');
 const { linkRedis } = require('./utility/redisConnection');
 require('dotenv').config();
-=======
-const express = require("express");
-const cors = require("cors");
-const {
-  registerUser,
-  loginUser,
-  forgotPassword,
-  getcurrentuser,
-  verifyOtp,
-  resendOtp,
-  changepassword,
-} = require("./controllers/userController");
-const { authentication } = require("./utility/passportUtility");
-const {
-  getAllSkills,
-  getMentorDetails,
-  getAllMentors,
-  getMentorProfile,
-} = require("./controllers/mentorController");
-const {
-  createWebinar,
-  getWebinars,
-  getAllWebinars,
-  addParticipant,
-  addHost,
-} = require("./controllers/dyteController");
-const {
-  getBlogs,
-  createBlog,
-  getCurrentBlog,
-  getAllBlogs,
-  deleteBlog,
-  addLike,
-  addComment,
-} = require("./controllers/blogController");
-require("dotenv").config();
->>>>>>> Stashed changes
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< Updated upstream
 linkRedis();
 
 app.post('/register', registerUser)
-=======
-app.post("/register", registerUser);
->>>>>>> Stashed changes
 
 app.post("/login", loginUser);
 
 app.post("/forgotpassword", forgotPassword);
 
-<<<<<<< Updated upstream
 app.post('/logout', authentication, logout)
 
 app.post('/changepassword', authentication, changepassword)
-=======
-app.post("/changepassword", authentication, changepassword);
->>>>>>> Stashed changes
 
 app.get("/getcurrentuser", authentication, getcurrentuser);
 
@@ -106,8 +60,8 @@ app.get("/getmentorprofile", authentication, getMentorProfile);
 
 app.post("/like", addLike);
 
-app.post("/comment", addComment);
+/* app.post("/comment", addComment); */
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
