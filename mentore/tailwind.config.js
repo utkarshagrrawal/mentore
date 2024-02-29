a16ff0e7a41ba914e89477bc4c628ae64ed8fd45
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-02-29 21:08:19 +0530

    Remove unused animation styles

--- mentore/tailwind.config.js
+++ mentore/tailwind.config.js
@@ -6,17 +6,7 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        blink: 'blink 1s linear infinite',
-      },
-      keyframes: {
-        blink: {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0 },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
