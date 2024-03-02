93974d31b7f089da5ef2dc1c95c14676a10bb33a
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-03-02 22:50:46 +0530

    Minor bug fixes

--- mentore/components/blog.jsx
+++ mentore/components/blog.jsx
@@ -24,6 +24,7 @@ export function Blog() {
             let users = await fetch("http://localhost:3000/getcurrentuser", options);
             const result = await users.json();
             if (result.error) {
+                navigate('/login');
                 setLoggedIn(false);
             } else {
                 setLoggedIn(true);
@@ -250,13 +251,13 @@ export function Blog() {
                                         </div>
                                         <div className="flex items-center gap-2">
                                             <div className="flex items-center">
-                                                <svg className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" onClick={() => handleLike(item.id)}>
+                                                <svg className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" onClick={() => handleLike(item.comment_id)}>
                                                     <path fill="currentColor" d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625zM15 12h-1v8h-4v-8H6.081L12 4.601L17.919 12z"></path>
                                                 </svg>
                                                 <span className="ml-1 text-sm text-gray-600">{item.likes || 0}</span>
                                             </div>
                                             <div className="flex items-center">
-                                                <svg className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" onClick={() => handleDislike(item.id)}>
+                                                <svg className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" onClick={() => handleDislike(item.comment_id)}>
                                                     <path fill="currentColor" d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059M12 19.399L6.081 12H10V4h4v8h3.919z"></path>
                                                 </svg>
                                                 <span className="ml-1 text-sm text-gray-600">{item.likes || 0}</span>
