eb1c999d066816a57eb41b212881cc2716de67de
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-03-04 21:03:33 +0530

    Updated dependencies and removed unused code

--- mentore/src/App.jsx
+++ mentore/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Login } from '../components/auth/login'
 import { Register } from '../components/auth/register'
 import { ForgotPassword } from '../components/auth/forgotPassword'
