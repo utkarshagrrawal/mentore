b3237a5dd4ddad6aca9e75c51d04c43b9e1b4c9d
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-04-21 09:39:12 +0530

    Refactor UI styles and update API endpoints

--- frontend/components/auth/forgotPassword/forgotPasswordPage.jsx
+++ frontend/components/auth/forgotPassword/forgotPasswordPage.jsx
@@ -43,8 +43,8 @@ export function ForgotPassword() {
     }
 
     return (
-        <div className='bg-[url("https://mwhhseuqzoudvibeyvrm.supabase.co/storage/v1/object/sign/images/bg.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvYmcuanBnIiwiaWF0IjoxNzExMTg4NzQ5LCJleHAiOjE3NDI3MjQ3NDl9.DxHTmGDNLY9mndHvmDPeCFY3htzmCC9W8n99fDV3PBE&t=2024-03-23T10%3A12%3A29.169Z")] bg-cover min-h-screen flex flex-col items-center justify-center px-6 py-12 lg:px-8'>
-            <Link to='/' className='relative'><img src="https://mwhhseuqzoudvibeyvrm.supabase.co/storage/v1/object/sign/images/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvbG9nby5wbmciLCJpYXQiOjE3MTIzNDQ0ODEsImV4cCI6MTc0Mzg4MDQ4MX0.R_a_H8TO26tJm794AbsCJzLN4tdFGyF2dIPrnYfWzzg&t=2024-04-05T19%3A14%3A41.780Z" className="h-12 mix-blend-multiply" alt="Mentore" /></Link>
+        <div className='bg-[url("https://static.vecteezy.com/system/resources/previews/001/437/080/non_2x/geometric-abstract-with-memphis-style-background-free-vector.jpg")] bg-cover min-h-screen flex flex-col items-center justify-center px-6 py-12 lg:px-8'>
+            <Link to='/' className='relative'><img src="https://mwhhseuqzoudvibeyvrm.supabase.co/storage/v1/object/sign/images/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvbG9nby5wbmciLCJpYXQiOjE3MTIzNDQ0ODEsImV4cCI6MTc0Mzg4MDQ4MX0.R_a_H8TO26tJm794AbsCJzLN4tdFGyF2dIPrnYfWzzg&t=2024-04-05T19%3A14%3A41.780Z" className="h-20 w-auto mix-blend-multiply" alt="Mentore" /></Link>
             {loading ? <Loader /> : emailSent ? <VerifyOtp handleChange={handleChange} setLoading={setLoading} forgotPassword={forgotPassword} /> : <RequestOtp handleChange={handleChange} setLoading={setLoading} setEmailSent={setEmailSent} forgotPassword={forgotPassword} />}
         </div>
     )
--- frontend/components/global/loader.jsx
+++ frontend/components/global/loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Loader() {
     return (
-        <div className="flex flex-row gap-2 z-999 absolute top-1/2 pt-10">
+        <div className="flex flex-row gap-2 z-999 absolute top-[54%] pt-10">
             <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce"></div>
             <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.3s]"></div>
             <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.5s]"></div>
