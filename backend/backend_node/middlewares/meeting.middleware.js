fff186688812d24dda39175db09361c2fd59900d
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-05-04 23:35:30 +0530

    chore: Update date handling in middleware files

--- backend/backend_node/middlewares/meeting.middleware.js
+++ backend/backend_node/middlewares/meeting.middleware.js
@@ -4,8 +4,12 @@ const { supabase } = require('../utility/database.connection')
 const removeExpiredMeetings = async (req, res, next) => {
     let now = new Date();
 
-    now.setHours(now.getHours() + 5);
-    now.setMinutes(now.getMinutes() + 30);
+    /* 
+        the below code is commented because the time is not in sync with the server time
+    */
+
+    // now.setHours(now.getHours() + 5);
+    // now.setMinutes(now.getMinutes() + 30);
 
     const { error } = await supabase
         .from('schedule_mentors')
--- backend/backend_node/middlewares/webinar.middleware.js
+++ backend/backend_node/middlewares/webinar.middleware.js
@@ -3,8 +3,12 @@ const { supabase } = require("../utility/database.connection");
 const removeExpiredWebinars = async (req, res, next) => {
     var now = new Date();
 
-    now.setHours(now.getHours() + 5);
-    now.setMinutes(now.getMinutes() + 30);
+    /* 
+        the below code is commented because the time is not in sync with the server time
+    */
+
+    // now.setHours(now.getHours() + 5);
+    // now.setMinutes(now.getMinutes() + 30);
 
     const { error } = await supabase
         .from('webinar')
